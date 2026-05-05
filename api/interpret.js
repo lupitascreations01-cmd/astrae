5a24c029c51268ac845c3c004022d7046dba2a82
lupitascreations01-cmd <lupitascreations.01@gmail.com>
2026-05-05 17:48:02 -0400

    Update interpret.js

--- api/interpret.js
+++ api/interpret.js
@@ -17,6 +17,10 @@ function checkRateLimit(ip) {
 
 module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+if (req.method === 'OPTIONS') return res.status(200).end();
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
